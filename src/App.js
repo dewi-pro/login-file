1c67b32cdfee52eac2fa6032f6d46ed015ba6542
haru <75671321+dewi-pro@users.noreply.github.com>
2021-01-26 13:20:44 +0700

    Add files via upload
    adding link to dashboard

--- src/App.js
+++ src/App.js
@@ -4,45 +4,51 @@ import './App.css';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
-
+import Dashboard from "./components/dashboard";
 import Login from "./components/login";
 import SignUp from "./components/signup";
+import Update from './components/dashboard/update';
 
 const client = new ApolloClient({
   uri: "https://eternal-goshawk-27.hasura.app/v1/graphql",
   cache: new InMemoryCache(),
+  headers: {
+    'x-hasura-admin-secret': 'hasura' // ini harusnya jwt
+  }
 });
 
 function App() {
   return (<Router>
     <ApolloProvider client={client}>
-    <div className="App">
-      <nav className="navbar navbar-expand-lg navbar-light fixed-top">
-        <div className="container">
-          <Link className="navbar-brand" to={"/sign-in"}>LogUp App</Link>
-          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-            <ul className="navbar-nav ml-auto">
-              <li className="nav-item">
-                <Link className="nav-link" to={"/sign-in"}>Sign in</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to={"/sign-up"}>Sign up</Link>
-              </li>
-            </ul>
+      <div className="App">
+        <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+          <div className="container">
+            <Link className="navbar-brand" to={"/sign-in"}>LogUp App</Link>
+            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+              <ul className="navbar-nav ml-auto">
+                <li className="nav-item">
+                  <Link className="nav-link" to={"/sign-in"}>Sign in</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to={"/sign-up"}>Sign up</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+
+        <div className="outer">
+          <div className="inner">
+            <Switch>
+              <Route exact path='/' component={Login} />
+              <Route path="/sign-in" component={Login} />
+              <Route path="/sign-up" component={SignUp} />
+              <Route path="/dash" component={Dashboard} />
+              <Route path="/update" component={Update} />
+            </Switch>
           </div>
         </div>
-      </nav>
-
-      <div className="outer">
-        <div className="inner">
-          <Switch>
-            <Route exact path='/' component={Login} />
-            <Route path="/sign-in" component={Login} />
-            <Route path="/sign-up" component={SignUp} />
-          </Switch>
-        </div>
-      </div>
-    </div></ApolloProvider></Router>
+      </div></ApolloProvider></Router>
   );
 }
 
