4b0efab24e1158324e4e546d1791b89dd9989489
haru <75671321+dewi-pro@users.noreply.github.com>
2021-01-26 13:27:23 +0700

    fixing reload

--- src/components/login.js
+++ src/components/login.js
@@ -1,81 +1,93 @@
 // eslint-disable-next-line
-import React,  { useState, useEffect } from "react";
-import Dashboard from "./dashboard";
-import { useMutation,useQuery } from "@apollo/client";
+import React, { useState, useEffect } from "react";
+import { useLazyQuery } from "@apollo/client";
 
-import { LOGIN_query, LOGIN} from "./graphql/queries";
-import {Col, Card, Alert, OverlayTrigger, Popover} from 'react-bootstrap';
+import { LOGIN_query } from "./graphql/queries";
+import { Col, OverlayTrigger, Popover } from 'react-bootstrap';
+import { useHistory } from "react-router-dom";
 
 
 export default () => {
+  const history = useHistory();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [screen, setScreen] = useState(false); 
 
-const LoginUser = () => {
-  const { loading, error, data } = useQuery(LOGIN_query);
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-  if (error) {
-    console.error(error);
-    return <div>Error!</div>;
-  }
-  return <Dashboard />;
-};
+  ///reff => https://www.apollographql.com/docs/react/data/queries/#executing-queries-manually
+  const [loginUser, { loading, error, data }] = useLazyQuery(LOGIN_query);
+
+  /// check if any change in query
+  useEffect(() => {
+    if (!data) {
+      if (loading) {
+        console.log('loading==>', loading)
+      }
+      if (error) {
+        console.error(error);
+      }
+    }
+    else {
+      console.log("data==>", data)
+      if(data.user.length === 0)
+        alert("not found")
+      else
+        //setScreen(true)
+        history.push('/dash')
+        ;
+    }
+  }, [data, loading, error])
 
   const SubmitTask = () => {
-    LoginUser({ variables: { email:'se@sa.cd', password:'123456' } });
-              };
-           
-        return (
-          <Col > 
-          {screen === false ? ( 
-                <form onSubmit={(e)=>{e.preventDefault();}}>                   
-                <div className="form-group">
-                    <label>Email</label>
-                    <input type="email" className="form-control" placeholder="Enter email" 
-                    value={email}
-                      onChange={e => setEmail(e.target.value)}
-                      type="email"
-                      required
-                                  />
-                </div>
+    console.log("submit task")
+    loginUser({ variables: { email: email, password: password } })
+  };
 
-                <div className="form-group">
-                    <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Enter password"
-                    value={password}
-                    onChange={e => setPassword(e.target.value)}
-                    required />
-                </div>
+  if (loading) {
+    return <div>Loading...</div>
+  }
 
-                <div className="form-group">
-                    <div className="custom-control custom-checkbox">
-                        <input type="checkbox" className="custom-control-input" id="customCheck1" />
-                        <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
-                    </div>
-                </div>
+  return (
+    <Col >
+        <form>
+          <div className="form-group">
+            <label>Email</label>
+            <input type="email" className="form-control" placeholder="Enter email"
+              value={email}
+              onChange={e => setEmail(e.target.value)}
+              required
+            />
+          </div>
 
-                <OverlayTrigger placement="right" overlay={
-                          !email || !password ? (
-                            <Popover id="Popover-disabled">Fill out the form to enable button !</Popover>
-                          ) : (
-                            <Popover id="Popover-disabled">Enabled!</Popover>
-                          )}>
-                          <button type="submit" className="btn btn-dark btn-lg btn-block" onClick={SubmitTask} disabled={!email || !password}>Sign in</button>
-                        </OverlayTrigger>
+          <div className="form-group">
+            <label>Password</label>
+            <input type="password" className="form-control" placeholder="Enter password"
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+              required />
+          </div>
 
-                
-                <p className="forgot-password text-right">
-                    Forgot <a href="#">password?</a>
-                </p>
-                
-                </form>
-                 ) : ( 
-                <Dashboard screen={screen} />
-            )}
-        </Col> 
-                       );
-                    
-                };
+          <div className="form-group">
+            <div className="custom-control custom-checkbox">
+              <input type="checkbox" className="custom-control-input" id="customCheck1" />
+              <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
+            </div>
+          </div>
+
+          <OverlayTrigger placement="right" overlay={
+            !email || !password ? (
+              <Popover id="Popover-disabled">Fill out the form to enable button !</Popover>
+            ) : (
+                <Popover id="Popover-disabled">Enabled!</Popover>
+              )}>
+            <button type="button" className="btn btn-dark btn-lg btn-block" onClick={() => { SubmitTask() }} disabled={!email || !password}>Sign in</button>
+          </OverlayTrigger>
+
+
+          <p className="forgot-password text-right">
+            Forgot <a href="#">password?</a>
+          </p>
+
+        </form>
+    </Col>
+  );
+
+};
