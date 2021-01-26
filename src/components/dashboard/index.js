bfc4fe8b7cd5b121fa87c497b1950275cae5b53f
haru <75671321+dewi-pro@users.noreply.github.com>
2021-01-26 13:23:51 +0700

    view user data and logout button

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,13 +1,44 @@
-import React, { Component } from 'react';
+import React from 'react'
+import {  useQuery } from '@apollo/client'
+import { Button, List } from '@material-ui/core'
 
-class Dashboard extends Component {
-    render() {
-        return (
-            <div>
-                <h2>welcome home</h2>
-            </div>
-        );
+import { GET_USER, REMOVE_USER } from '../graphql/queries'
+
+export const Dashboard = ({ history }) => {
+  const { loading, error, data } = useQuery(GET_USER, {
+    context: {
+      headers: {
+        'x-hasura-admin-secret': 'hasura' // ini harusnya jwt
+      }
     }
-}
+  })
 
+  if (loading) return 'loading ...'
+
+  if (error) return `Error: ${error.message}`
+
+  return (
+    <>
+              <Button color="primary" 
+              onClick={() => history.push("/")}
+              >Logout  </Button>
+              
+      {data.user.map(users =>
+        <div key={users.id} className='row'>
+          <div className='column' />
+          <div className='column' style={{ textAlign: 'center' }}>
+              <br></br>
+             <div > 
+             <List>{users.first_name}  {users.last_name} </List> 
+             <Button color="primary" >UPDATE</Button>
+             <Button color="primary" >DELETE</Button>
+             </div>
+          </div>
+          <div className='column' />
+        </div>
+      )}
+    </>
+
+  )
+}
 export default Dashboard;
