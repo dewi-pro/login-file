35ee57ce22ce7cb870619559df37f0d7cc6bfb59
haru <75671321+dewi-pro@users.noreply.github.com>
2021-01-29 09:52:20 +0700

    Add files via upload

--- src/components/graphql/queries.js
+++ src/components/graphql/queries.js
@@ -3,6 +3,7 @@ import { gql } from "@apollo/client";
 export const GET_USER= gql`
   query getuser {
     user {
+        id
         first_name
         last_name
         email
@@ -11,12 +12,26 @@ export const GET_USER= gql`
   }
 `;
 
+export const GET_USERs = gql`
+query MyQuery($id: Int!) {
+  user(where: {id: {_eq: $id}}) 
+  {
+    id
+    first_name
+    last_name
+    email
+    password 
+  }
+}
+`;
+
 export const ADD_USER = gql`
   mutation($first_name: String!,$last_name: String!,$email: String!,$password: String!) {
     insert_user(objects: [{ first_name: $first_name, last_name: $last_name, email: $email, password: $password }]) 
     {
         affected_rows
         returning {
+          id
           first_name
           last_name
           email
@@ -26,40 +41,19 @@ export const ADD_USER = gql`
   }
 `;
 
-export const TOGGLE_COMPLETED = gql`
-  mutation($id: Int!,$successed: Boolean!) {
-    update_user_by_pk(
-      pk_columns: { id: $id }
-      _set: { successed: $successed }
-    ) {
-      id
-    }
-  }
-`;
-
 export const REMOVE_USER = gql`
-  mutation($id: Int!) {
-    delete_user_by_pk(id: $id) {
-      id
+mutation ($id: Int) {
+      delete_user (
+        where: {
+          id: {
+            _eq: $id
+          }
+        }
+      ) {
+        affected_rows
+      }
     }
-  }
-`;
-
-export const LOGIN = gql`
-query ($email: String!, $password: String!) {
-  user
-  ( where: 
-    {
-      email: {email: $email}, 
-      password: {password: $password}
-    }
-  ) 
-  {
-    email
-    password
-  }
-}
-`;
+  `;
 
 export const LOGIN_query = gql`
   query MyQuery($email: String!, $password: String!) {
@@ -67,6 +61,24 @@ export const LOGIN_query = gql`
     email
     password
     id
+  }
+}
+`;
+export const UPDATE = gql`
+mutation($id: Int!,$first_name: String!,$last_name: String!,$email: String!,$password: String!) {
+  update_user(
+    where: {id: {_eq: $id}},
+    _set: {
+      first_name: $first_name,
+      last_name: $last_name, 
+      email: $email, 
+      password: $password
+    }
+    )
+  {
+      returning {
+        id
+      }
   }
 }
 `;
